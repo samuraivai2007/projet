c5feec74966821c7bed52baab7b2d31331e03cdd
samuraivai2007 <86715086+samuraivai2007@users.noreply.github.com>
2021-07-04 17:24:52 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,29 @@
+
+function setup() {
+  createCanvas(400,400);
+}
+
+function draw() 
+{
+  background(30);
+
+  if(keyIsDown(DOWN_ARROW)){
+    background('red')
+  }
+    if(KeyIsDown(UP_ARROW)){
+      background('green')
+    }
+      if(KeyIsDown(RIGHT_ARROW)){
+  background('blue')
+      }
+      if(KeyIsDown(LEFT_ARROW)){
+        background('black')
+      }
+    
+  
+
+}
+
+
+
+
